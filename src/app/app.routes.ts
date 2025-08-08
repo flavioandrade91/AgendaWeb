4c50e9b065200985475d4e101d1e8c9ebed047ac
flavioandrade91 <flavio.andrade91@outlook.com.br>
2025-08-07 21:50:15 -0300

    first commmit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { CadastroTarefas } from './components/pages/cadastro-tarefas/cadastro-tarefas';
+import { ConsultaTarefas } from './components/pages/consulta-tarefas/consulta-tarefas';
+import { EdicaoTarefas } from './components/pages/edicao-tarefas/edicao-tarefas';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/pages/consulta-tarefas',
+  },
+  {
+    path: 'pages/cadastro-tarefas',
+    component: CadastroTarefas,
+  },
+  {
+    path: 'pages/consulta-tarefas',
+    component: ConsultaTarefas,
+  },
+  {
+    path: 'pages/edicao-tarefas',
+    component: EdicaoTarefas,
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/shared/navbar/navbar';
+import { CadastroTarefas } from './components/pages/cadastro-tarefas/cadastro-tarefas';
+import { EdicaoTarefas } from './components/pages/edicao-tarefas/edicao-tarefas';
+import { ConsultaTarefas } from './components/pages/consulta-tarefas/consulta-tarefas';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Navbar,
+    CadastroTarefas,
+    EdicaoTarefas,
+    ConsultaTarefas,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('AgendaWeb');
-}
+export class App {}
