af2b82924e34e8f730fb3307d8d773a29fa471de
flavioandrade91 <flavio.andrade91@outlook.com.br>
2025-08-14 21:48:35 -0300

    feat:integração basica do back com front

--- src/app/components/pages/consulta-tarefas/consulta-tarefas.ts
+++ src/app/components/pages/consulta-tarefas/consulta-tarefas.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-consulta-tarefas',
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './consulta-tarefas.html',
-  styleUrl: './consulta-tarefas.css'
+  styleUrl: './consulta-tarefas.css',
 })
 export class ConsultaTarefas {
+  //atributos
+  private http = inject(HttpClient);
 
+  //Criando um formulário reativo
+  formulario = new FormGroup({
+    dataHoraInicio: new FormControl(''),
+    dataHoraFim: new FormControl(''),
+  });
+
+  //funcao para captura o submit
+  onsubmit() {
+    //capturando os dados do formulário
+    const dataHoraInicio = this.formulario.value.dataHoraInicio;
+    const dataHoraFim = this.formulario.value.dataHoraFim;
+
+    //enviar uma requisição HTTP
+    this.http
+      .get(
+        `http://localhost:5170/api/tarefas/` +
+          dataHoraInicio +
+          `/` +
+          dataHoraFim
+      )
+      .subscribe((response) => {
+        console.table(response);
+      });
+  }
 }
